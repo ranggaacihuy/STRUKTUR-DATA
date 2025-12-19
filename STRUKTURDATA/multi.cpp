a202c5efcfc31b42ee90e63b2aa4d1fe1527788d
ranggaacihuy <ranggadani2005@gmail.com>
2025-12-19 20:30:24 +0700

    Refactor multi.cpp for improved structure and clarity

--- STRUKTURDATA/multi.cpp
+++ STRUKTURDATA/multi.cpp
@@ -1,6 +1,6 @@
 #include "multi.h"
 
-//create list
+// Create List
 void createListPenyakit(ListPenyakit &LP) {
     LP.first = nullptr;
     LP.last = nullptr;
@@ -8,13 +8,14 @@ void createListPenyakit(ListPenyakit &LP) {
 
 void createListPasien(ListPasien &LS) {
     LS.first = nullptr;
+    LS.last = nullptr;
 }
 
 void createListRelasi(ListRelasi &LR) {
     LR.first = nullptr;
 }
 
-//create node
+// Create Node
 adrPenyakit createElmPenyakit(infotypeP x) {
     adrPenyakit P = new elmPenyakit;
     P->info = x;
@@ -27,6 +28,7 @@ adrPasien createElmPasien(infotypeS x) {
     adrPasien P = new elmPasien;
     P->info = x;
     P->next = nullptr;
+    P->prev = nullptr;
     return P;
 }
 
@@ -38,65 +40,59 @@ adrRelasi createElmRelasi(adrPasien P, adrPenyakit Q) {
     return R;
 }
 
-//insert list
+// Insert
 void insertPenyakit(ListPenyakit &LP, adrPenyakit P) {
     if (LP.first == nullptr) {
-        LP.first = P;
-        LP.last = P;
+        LP.first = LP.last = P;
     } else {
         LP.last->next = P;
+        P->prev = LP.last;
         LP.last = P;
     }
 }
 
 void insertPasien(ListPasien &LS, adrPasien S) {
     if (LS.first == nullptr) {
-        LS.first = S;
+        LS.first = LS.last = S;
     } else {
         S->next = LS.first;
+        LS.first->prev = S;
         LS.first = S;
     }
 }
 
 void insertRelasi(ListRelasi &LR, adrRelasi R) {
-    if (LR.first == nullptr) {
-        LR.first = R;
-    } else {
-        R->next = LR.first;
-        LR.first = R;
-    }
+    R->next = LR.first;
+    LR.first = R;
 }
 
-// delete
+// Delete
 void deletePasien(ListPasien &LS, ListRelasi &LR, string namaPasien) {
     adrPasien P = LS.first;
-    adrPasien prevP = nullptr;
 
-    while (P != nullptr && P->info.nama != namaPasien) {
-        prevP = P;
+    while (P != nullptr && P->info.namaPasien != namaPasien) {
         P = P->next;
     }
 
     if (P == nullptr) {
-        cout << "Pasien tidak ditemukan" << endl;
+        cout << "Pasien tidak ditemukan\n";
         return;
     }
 
+    // hapus relasi
     adrRelasi R = LR.first;
     adrRelasi prevR = nullptr;
 
     while (R != nullptr) {
         if (R->pasien == P) {
             adrRelasi temp = R;
-
             if (prevR == nullptr) {
                 LR.first = R->next;
-                R = LR.first;
             } else {
                 prevR->next = R->next;
-                R = prevR->next;
             }
-
+            R = R->next;
+            temp->penyakit->info.penderita--;
             delete temp;
         } else {
             prevR = R;
@@ -104,43 +100,45 @@ void deletePasien(ListPasien &LS, ListRelasi &LR, string namaPasien) {
         }
     }
 
-    if (prevP == nullptr) {
+    // hapus pasien
+    if (P->prev != nullptr)
+        P->prev->next = P->next;
+    else
         LS.first = P->next;
-    } else {
-        prevP->next = P->next;
-    }
+
+    if (P->next != nullptr)
+        P->next->prev = P->prev;
+    else
+        LS.last = P->prev;
 
     delete P;
-    cout << "Pasien berhasil dihapus" << endl;
 }
 
 void deletePenyakit(ListPenyakit &LP, ListRelasi &LR, string namaPenyakit) {
-     adrPenyakit Q = LP.first;
+    adrPenyakit Q = LP.first;
 
-    while (Q != nullptr && Q->info.nama != namaPenyakit) {
+    while (Q != nullptr && Q->info.namaPenyakit != namaPenyakit) {
         Q = Q->next;
     }
 
     if (Q == nullptr) {
-        cout << "Penyakit tidak ditemukan" << endl;
+        cout << "Penyakit tidak ditemukan\n";
         return;
     }
 
+    // hapus relasi
     adrRelasi R = LR.first;
     adrRelasi prevR = nullptr;
 
     while (R != nullptr) {
         if (R->penyakit == Q) {
             adrRelasi temp = R;
-
-            if (prevR == nullptr) {
+            if (prevR == nullptr)
                 LR.first = R->next;
-                R = LR.first;
-            } else {
+            else
                 prevR->next = R->next;
-                R = prevR->next;
-            }
 
+            R = R->next;
             delete temp;
         } else {
             prevR = R;
@@ -148,29 +146,26 @@ void deletePenyakit(ListPenyakit &LP, ListRelasi &LR, string namaPenyakit) {
         }
     }
 
-    if (Q->prev != nullptr) {
+    // hapus penyakit
+    if (Q->prev != nullptr)
         Q->prev->next = Q->next;
-    } else {
+    else
         LP.first = Q->next;
-    }
 
-    if (Q->next != nullptr) {
+    if (Q->next != nullptr)
         Q->next->prev = Q->prev;
-    } else {
+    else
         LP.last = Q->prev;
-    }
 
     delete Q;
-    cout << "Penyakit berhasil dihapus" << endl;
 }
 
-// search
+// Search
 adrPasien findPasien(const ListPasien &LS, string nama) {
     adrPasien P = LS.first;
     while (P != nullptr) {
-        if (P->info.nama == nama) {
+        if (P->info.namaPasien == nama)
             return P;
-        }
         P = P->next;
     }
     return nullptr;
@@ -178,87 +173,68 @@ adrPasien findPasien(const ListPasien &LS, string nama) {
 
 adrPenyakit findPenyakit(const ListPenyakit &LP, string nama) {
     adrPenyakit P = LP.first;
-
     while (P != nullptr) {
-        if (P->info.nama == nama) {
+        if (P->info.namaPenyakit == nama)
             return P;
-        }
         P = P->next;
     }
     return nullptr;
 }
 
-// show
+// Show
 void showAllPasienPenyakit(const ListPasien &LS, const ListRelasi &LR) {
     adrPasien P = LS.first;
 
     while (P != nullptr) {
-        cout << "Pasien : " << P->info.nama << endl;
-        cout << "Penyakit : ";
+        cout << "Pasien: " << P->info.namaPasien << endl;
+        cout << "Penyakit: ";
 
         adrRelasi R = LR.first;
         bool ada = false;
 
         while (R != nullptr) {
             if (R->pasien == P) {
-                cout << R->penyakit->info.nama << ", ";
+                cout << R->penyakit->info.namaPenyakit << ", ";
                 ada = true;
             }
             R = R->next;
         }
 
-        if (!ada) {
-            cout << "-";
-        }
-
-        cout << endl << "------------------" << endl;
+        if (!ada) cout << "-";
+        cout << endl << "------------------\n";
         P = P->next;
     }
 }
 
 void showPasienDenganPenyakit(const ListRelasi &LR, string namaPenyakit) {
     adrRelasi R = LR.first;
-    bool ketemu = false;
-    cout << "Pasien dengan penyakit " << namaPenyakit << ":" << endl;
     while (R != nullptr) {
-        if (R->penyakit->info.nama == namaPenyakit) {
-            cout << "- " << R->pasien->info.nama << endl;
-            ketemu = true;
+        if (R->penyakit->info.namaPenyakit == namaPenyakit) {
+            cout << R->pasien->info.namaPasien << endl;
         }
         R = R->next;
-    }
-    if (!ketemu) {
-        cout << "Tidak ada pasien dengan penyakit tersebut." << endl;
     }
 }
 
 void showPenyakitPadaPasien(const ListRelasi &LR, string namaPasien) {
     adrRelasi R = LR.first;
-    bool ketemu = false;
-    cout << "Penyakit yang dimiliki pasien " << namaPasien << ":" << endl;
     while (R != nullptr) {
-        if (R->pasien->info.nama == namaPasien) {
-            cout << "- " << R->penyakit->info.nama << endl;
-            ketemu = true;
+        if (R->pasien->info.namaPasien == namaPasien) {
+            cout << R->penyakit->info.namaPenyakit << endl;
         }
         R = R->next;
-    }
-    if (!ketemu) {
-        cout << "Pasien tidak memiliki penyakit atau tidak ditemukan." << endl;
     }
 }
 
 void showTop5Penyakit(const ListPenyakit &LP) {
-    adrPenyakit P;
-     adrPenyakit top[5] = {nullptr, nullptr, nullptr, nullptr, nullptr};
+    adrPenyakit top[5] = {nullptr};
 
-    P = LP.first;
+    adrPenyakit P = LP.first;
     while (P != nullptr) {
         for (int i = 0; i < 5; i++) {
-            if (top[i] == nullptr || P->info.jumlah > top[i]->info.jumlah) {
-                for (int j = 4; j > i; j--) {
+            if (top[i] == nullptr || P->info.penderita > top[i]->info.penderita) {
+                for (int j = 4; j > i; j--)
                     top[j] = top[j - 1];
-                }
                 top[i] = P;
                 break;
             }
@@ -266,32 +242,24 @@ void showTop5Penyakit(const ListPenyakit &LP) {
         P = P->next;
     }
 
-    cout << "Top 5 Penyakit Terbanyak:" << endl;
+    cout << "Top 5 Penyakit:\n";
     for (int i = 0; i < 5; i++) {
         if (top[i] != nullptr) {
             cout << i + 1 << ". "
-                 << top[i]->info.nama
-                 << " (" << top[i]->info.jumlah << " pasien)" << endl;
+                 << top[i]->info.namaPenyakit
+                 << " (" << top[i]->info.penderita << " pasien)\n";
         }
     }
 }
 
-// relasi
+// Relasi
 void relasiPasienDanPenyakit(ListRelasi &LR, adrPasien P, adrPenyakit Q) {
-     if (P == nullptr || Q == nullptr) {
-        cout << "Data pasien atau penyakit tidak ditemukan" << endl;
+    if (P == nullptr || Q == nullptr) {
+        cout << "Relasi gagal\n";
         return;
     }
 
     adrRelasi R = createElmRelasi(P, Q);
-
-    if (LR.first == nullptr) {
-        LR.first = R;
-    } else {
-        R->next = LR.first;
-        LR.first = R;
-    }
-
-    Q->info.jumlah++;
+    insertRelasi(LR, R);
+    Q->info.penderita++;
 }
-
